3ba8ceeb3cefbe96a05d44b1c6b37f780a9d5f3a
sndesantis <sarahndesantis@gmail.com>
2023-04-01 16:20:44 -0600

    Updates

---
+++ precache-manifest.560a5e3ab49b650d1c06bf84a6fa95d8.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "b8c5f668c0a0eab38999",
+    "url": "/sarahreactportfolio/static/css/main.f1d0f72e.chunk.css"
+  },
+  {
+    "revision": "b8c5f668c0a0eab38999",
+    "url": "/sarahreactportfolio/static/js/main.b8c5f668.chunk.js"
+  },
+  {
+    "revision": "9594a6bb494a9d9ba425",
+    "url": "/sarahreactportfolio/static/js/1.9594a6bb.chunk.js"
+  },
+  {
+    "revision": "1a9ab0a0c9386adf8f59",
+    "url": "/sarahreactportfolio/static/js/2.1a9ab0a0.chunk.js"
+  },
+  {
+    "revision": "2b2e6556b6d5dd5e2d6b",
+    "url": "/sarahreactportfolio/static/js/runtime~main.2b2e6556.js"
+  },
+  {
+    "revision": "531ff8b67fa9e390a394d3fc4200c051",
+    "url": "/sarahreactportfolio/static/media/image2.531ff8b6.jpeg"
+  },
+  {
+    "revision": "4271f12d9d4de47b01853e9711dcdeeb",
+    "url": "/sarahreactportfolio/static/media/algorhythym.4271f12d.png"
+  },
+  {
+    "revision": "59e33bdaeed81ca842585c78c16fc1a6",
+    "url": "/sarahreactportfolio/static/media/thesuitelife.59e33bda.png"
+  },
+  {
+    "revision": "b3ca119960cdbf521cf39002eea8dc2e",
+    "url": "/sarahreactportfolio/static/media/coming-soon.b3ca1199.png"
+  },
+  {
+    "revision": "67db642019cd2c5ccc27279915bc6dda",
+    "url": "/sarahreactportfolio/static/media/dark-black-stripes-black-and-white-590013114.67db6420.jpg"
+  },
+  {
+    "revision": "173b341d7563bd53a9424ca38190052b",
+    "url": "/sarahreactportfolio/index.html"
+  }
+];
